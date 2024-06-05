25882c50964bec031f2f957208bca7bbebe61d44
Davidm <david.mueller2707@gmail.com>
2024-06-05 08:15:29 +0200

    Abschluss

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Willkommen zum Kino-Buchungs-Management-System");
+        System.out.println("Willkommen bei mega ultra Kinobesuch");
 
         try (ConnectionSource connectionSource = new JdbcConnectionSource(DATABASE_URL)) {
             DatabaseManager.createTables(connectionSource);
